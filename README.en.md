b0ee7fc98af10b0253c71ca60358ad34ead3fcc3
1090414202 <1090414202@qq.com>
2021-09-29 17:14:09 +0800

    类加载器扫描context下的包

--- README.en.md
+++ README.en.md
@@ -2,12 +2,12 @@
 
 #### 实现内容包括
 - spring启动及扫描流程 
-- getBean（）流程 
-  Bean生命周期流程 
-  依赖注入流程
-  BeanPostProcessor机制
-  Aop机制
-  getter，setter注释封装
+- getBean（）流程
+- Bean生命周期流程
+- 依赖注入流程
+- BeanPostProcessor机制
+- Aop机制
+- getter，setter注释封装
   
 >相关说明
 > @Retention(RetentionPolicy.RUNTIME) 注释 规定注释存在的生命周期
--- src/main/java/com/ni/sourceCode/spring/ApplicationContext.java
+++ src/main/java/com/ni/sourceCode/spring/ApplicationContext.java
@@ -9,7 +9,14 @@ public class ApplicationContext {
 
         this.configClass = configClass;
 //        解析配置类
-//        ComponentScan注解-->扫描路径-->扫描
+//        ComponentScan注解-->扫描路径-->扫描路径包下所有有spring注释中的类，对其进行解析
+//        获取传入类的componentScan注解
+        ComponentScan componentScanAnnotation = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
+        String path = componentScanAnnotation.value();
+        System.out.println(path);
+
+//        扫描路径
+
     }
 
     public Object getBean(String beanName) {
